34dcb8652b0ac82f5f14f78076c2c85e39275da2
Antonio Canno <antonio.canno257@gmail.com>
2020-11-17 15:49:43 -0300

    primeiro commit

---
+++ src/main/java/com/example/poo_projeto_final/PooProjetoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.poo_projeto_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooProjetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooProjetoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/poo_projeto_final/PooProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poo_projeto_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
